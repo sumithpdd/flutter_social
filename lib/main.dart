cf8898d87c1c51cff97f55b638d9865629e68019
Sumithpd <https://github.com/sumithpdd/flutter_complete_guide.git>
2020-05-09 11:42:03 +0200

    Login Screen with login buttons

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_social_ui/screens/login_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Social',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
+ 
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+          child: Container(
+        height: MediaQuery.of(context).size.height,
+        child: Column(children: <Widget>[
+          Image(
+            height: MediaQuery.of(context).size.height / 2.5,
+            width: double.infinity,
+            image: AssetImage('assets/images/login_background.jpg'),
+            fit: BoxFit.cover,
+          ),
+          Text(
+            'FRENZY',
+            style: TextStyle(
+                color: Theme.of(context).primaryColor,
+                fontSize: 34.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1.0),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                fillColor: Colors.white,
+                filled: true,
+                hintText: 'Username',
+                prefixIcon: Icon(
+                  Icons.account_box,
+                  size: 30,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                fillColor: Colors.white,
+                filled: true,
+                hintText: 'Password',
+                prefixIcon: Icon(
+                  Icons.lock,
+                  size: 30,
+                ),
+              ),
+              obscureText: true,
+            ),
+          ),
+          SizedBox(
+            height: 40.0,
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 60.0),
+              alignment: Alignment.center,
+              height: 45.0,
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Text(
+                'Login',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: 1.5),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Align(
+              alignment: FractionalOffset.bottomCenter,
+              child: GestureDetector(
+                onTap: () {},
+                child: Container(
+                  alignment: Alignment.center,
+                  color: Theme.of(context).primaryColor,
+                  height: 80.0,
+                  child: Text(
+                    'Don\'t have an account? Sign up',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ]),
+      )),
+    );
+  }
+}
